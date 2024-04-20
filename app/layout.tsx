57195a1012d62314367db93750949f9447abb2b6
Chanin Law <chaninlawlert@gmail.com>
2024-04-20 23:53:17 +0700

    fix: import style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-// import '@/env'
+import '@/styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
-import './globals.css'
 import { cn } from '@/lib/utils'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Navbar } from '@/components/navbar'
