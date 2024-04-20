37c03dea8b06814e1a9d12392f970052ebcf1d52
Chanin Law <chaninlawlert@gmail.com>
2024-04-20 23:54:14 +0700

    migrate: type and key

--- server/db/schema/index.ts
+++ server/db/schema/index.ts
@@ -1,12 +1,12 @@
 import { relations } from 'drizzle-orm'
-import { pgTableCreator, serial, boolean, index, text, timestamp, varchar } from 'drizzle-orm/pg-core'
+import { pgTableCreator, index, text, timestamp, varchar } from 'drizzle-orm/pg-core'
 import { DATABASE_PREFIX as prefix } from '@/lib/constants'
 
 export const pgTable = pgTableCreator((name) => `${prefix}_${name}`)
 
 export const users = pgTable('users', {
-  id: varchar('id', { length: 21 }).primaryKey(),
-  github_id: varchar('discord_id', { length: 255 }).unique(),
+  id: varchar('id', { length: 255 }).primaryKey(),
+  githubId: varchar('github_id', { length: 255 }).unique(),
   username: varchar('username', { length: 255 }),
   email: varchar('email', { length: 255 }),
   avatar: varchar('avatar', { length: 255 }),
@@ -21,7 +21,9 @@ export const sessions = pgTable(
   'sessions',
   {
     id: varchar('id', { length: 255 }).primaryKey(),
-    userId: varchar('user_id', { length: 21 }).notNull(),
+    userId: varchar('user_id', { length: 255 })
+      .notNull()
+      .references(() => users.id),
     expiresAt: timestamp('expires_at', {
       withTimezone: true,
       mode: 'date'
@@ -35,22 +37,31 @@ export const sessions = pgTable(
 export const comment = pgTable(
   'comment',
   {
-    id: varchar('id', { length: 15 }).primaryKey(),
-    userId: varchar('user_id', { length: 255 }).notNull(),
-    text: text('text').notNull(),
+    id: varchar('id', { length: 255 }).primaryKey(),
+    userId: varchar('user_id', { length: 255 })
+      .notNull()
+      .references(() => users.id),
+    content: text('content').notNull(),
     createdAt: timestamp('created_at').defaultNow().notNull(),
-    updatedAt: timestamp('updated_at', { mode: 'date' }).$onUpdate(() => new Date())
+    updatedAt: timestamp('updated_at', { mode: 'date' }).$onUpdate(() => new Date()),
+    parentId: varchar('parent_id', { length: 15 })
   },
   (t) => ({
-    userIdx: index('comment_user_idx').on(t.userId),
-    createdAtIdx: index('comment_created_at_idx').on(t.createdAt)
+    userIdx: index('comment_user_idx').on(t.userId)
   })
 )
 
-export const postRelations = relations(comment, ({ one }) => ({
+export const commentRelations = relations(comment, ({ one, many }) => ({
   user: one(users, {
     fields: [comment.userId],
     references: [users.id]
+  }),
+  parent: one(comment, {
+    fields: [comment.parentId],
+    references: [comment.id]
+  }),
+  children: many(comment, {
+    relationName: 'children'
   })
 }))
 
