79a61a12ec1d8d62b5fdd09a69753c81e3e1a061
Chanin Law <chaninlawlert@gmail.com>
2024-04-21 21:57:13 +0700

    add: vercel system environment

--- lib/client.ts
+++ lib/client.ts
@@ -3,8 +3,8 @@ import { env } from '@/env'
 export const client = (endpoint: string, init?: RequestInit) => {
   let baseUrl: string | undefined = ''
 
-  if (env.NEXT_PUBLIC_NODE_ENV === 'development') {
-    baseUrl = env.NEXT_PUBLIC_APP_URL
+  if (env.NEXT_PUBLIC_VERCEL_ENV === 'development') {
+    baseUrl = env.NEXT_PUBLIC_VERCEL_URL
   }
 
   return fetch(`${baseUrl}${endpoint}`, init)
