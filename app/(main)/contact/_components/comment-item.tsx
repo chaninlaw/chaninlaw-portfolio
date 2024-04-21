c3a89ccb6290dc4dc2507ba6c585bd5dc1a77386
Chanin Law <chaninlawlert@gmail.com>
2024-04-21 22:16:05 +0700

    mod: hide reply comment

--- app/(main)/contact/_components/comment-item.tsx
+++ app/(main)/contact/_components/comment-item.tsx
@@ -9,18 +9,18 @@ import { useComment } from '../_context'
 import { MotionDiv } from '@/components/ui/motion-div'
 
 export function CommentItem({ comment, avatarClassName, ...props }: CommentItemProps) {
-  const { state, dispatch } = useComment()
+  // const { state, dispatch } = useComment()
 
-  const handleReply = (id: string) => {
-    if (state.activeCommentId === id) {
-      dispatch({ type: 'CLOSE_COMMENT_BOX' })
-    } else {
-      dispatch({ type: 'OPEN_COMMENT_BOX', commentId: id })
-    }
-  }
+  // const handleReply = (id: string) => {
+  //   if (state.activeCommentId === id) {
+  //     dispatch({ type: 'CLOSE_COMMENT_BOX' })
+  //   } else {
+  //     dispatch({ type: 'OPEN_COMMENT_BOX', commentId: id })
+  //   }
+  // }
 
   return (
-    <div {...props} className='flex items-start gap-4 mt-2 border-l-2 border-gray-200 pl-4 dark:border-white/50'>
+    <div {...props} className='flex items-start gap-4 p-3 border-l-2 border-gray-200 pl-4 dark:border-white/50'>
       <Avatar className={cn('h-8 w-8', avatarClassName)}>
         <AvatarImage alt={comment.author?.username || undefined} src={comment.author?.avatar || undefined} />
         <AvatarFallback>{comment.author?.username?.slice(0, 2) ?? 'AN'}</AvatarFallback>
@@ -32,20 +32,20 @@ export function CommentItem({ comment, avatarClassName, ...props }: CommentItemP
             <div className='text-xs text-gray-500 dark:text-gray-400'>{timeAgo(comment.createdAt)}</div>
           </div>
           <div className='flex items-center gap-2'>
-            <Button size='sm' variant='ghost' onClick={() => handleReply(comment.id)}>
+            {/* <Button size='sm' variant='ghost' onClick={() => handleReply(comment.id)}>
               Reply
-            </Button>
+            </Button> */}
           </div>
         </div>
         <p>{comment.text}</p>
-        {state.activeCommentId === comment.id && (
+        {/* {state.activeCommentId === comment.id && (
           <MotionDiv className='mt-4' initial={{ y: -10 }} animate={{ y: 0 }}>
             <Textarea className='min-h-[100px]' placeholder='Write your reply...' />
             <div className='mt-2 flex justify-end'>
               <Button size='sm'>Submit</Button>
             </div>
           </MotionDiv>
-        )}
+        )} */}
       </div>
     </div>
   )
