3b3c396a680ff211d42e29c399600ec9a982f752
Chanin Law <chaninlawlert@gmail.com>
2024-04-23 00:27:04 +0700

    update: middleware

--- middleware.ts
+++ middleware.ts
@@ -7,13 +7,13 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
   if (request.method === 'GET') {
     return NextResponse.next()
   }
-  // const originHeader = request.headers.get('Origin')
-  // const hostHeader = request.headers.get('Host')
-  // if (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [hostHeader])) {
-  //   return new NextResponse(null, {
-  //     status: 403
-  //   })
-  // }
+  const originHeader = request.headers.get('Origin')
+  const hostHeader = request.headers.get('Host')
+  if (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [hostHeader])) {
+    return new NextResponse(null, {
+      status: 403
+    })
+  }
   return NextResponse.next()
 }
 
