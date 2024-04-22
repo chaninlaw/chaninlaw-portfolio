3259211d1a0d915198e8591861dac6b1bbb64fdf
Chanin Law <chaninlawlert@gmail.com>
2024-04-23 00:30:02 +0700

    feat: sending email

--- app/(main)/contact/_actions/index.ts
+++ app/(main)/contact/_actions/index.ts
@@ -3,9 +3,13 @@
 import { z } from 'zod'
 import { db } from '@/server/db'
 import { revalidatePath } from 'next/cache'
+import { env } from '@/env'
 
-import { comments, posts } from '@/server/db/schema'
-import { createPostSchema, replyPostSchema } from './validation'
+import { Resend } from 'resend'
+import WelcomeEmail from '@/emails/welcome'
+
+import { comments, emails, posts } from '@/server/db/schema'
+import { createPostSchema, replyPostSchema, sendEmailSchema } from './validation'
 import type { ActionResult } from '@/types/serverAction'
 
 export const createPost = async (data: z.infer<typeof createPostSchema>): Promise<ActionResult> => {
@@ -21,7 +25,7 @@ export const createPost = async (data: z.infer<typeof createPostSchema>): Promis
   const { authorId, content } = parsed.data
 
   try {
-    await db.insert(posts).values({ authorId, content }).returning()
+    await db.insert(posts).values({ authorId, content })
   } catch (error) {
     if (error instanceof Error) {
       return {
@@ -33,7 +37,8 @@ export const createPost = async (data: z.infer<typeof createPostSchema>): Promis
 
   revalidatePath('/contact')
   return {
-    success: true
+    success: true,
+    message: 'Your post has been sent.'
   }
 }
 
@@ -50,7 +55,7 @@ export const replyPost = async (data: z.infer<typeof replyPostSchema>): Promise<
   const { authorId, text, postId } = parsed.data
 
   try {
-    await db.insert(comments).values({ authorId, postId, text }).returning()
+    await db.insert(comments).values({ authorId, postId, text })
   } catch (error) {
     if (error instanceof Error) {
       return {
@@ -62,6 +67,51 @@ export const replyPost = async (data: z.infer<typeof replyPostSchema>): Promise<
 
   revalidatePath('/contact')
   return {
-    success: true
+    success: true,
+    message: 'Your comment has been sent.'
+  }
+}
+
+export const sendEmail = async (prevState: ActionResult, formData: FormData): Promise<ActionResult> => {
+  const parsed = sendEmailSchema.safeParse(Object.fromEntries(formData))
+
+  if (!parsed.success) {
+    return {
+      success: false,
+      message: parsed.error.message
+    }
+  }
+  const { name, email, message } = parsed.data
+
+  try {
+    await db.insert(emails).values({ email, message, name })
+
+    const resend = new Resend(env.RESEND_API_KEY)
+
+    const data = await resend.emails.send({
+      from: 'Chanin L. <onboarding@resend.dev>',
+      to: [email],
+      subject: "Let's Connect!",
+      react: WelcomeEmail({ name })
+    })
+
+    if (data.error) {
+      return {
+        success: false,
+        message: data.error.message
+      }
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        success: false,
+        message: error.message
+      }
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Your email has been sent.'
   }
 }
--- app/(main)/contact/_components/get-in-touch.tsx
+++ app/(main)/contact/_components/get-in-touch.tsx
@@ -1,11 +1,29 @@
+'use client'
+import * as React from 'react'
+import Link from 'next/link'
+import { useFormState } from 'react-dom'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import { GitHubLogoIcon, LinkedInLogoIcon, TwitterLogoIcon } from '@radix-ui/react-icons'
+import { SubmitButton } from '@/components/button/submit-button'
+
+import { sendEmail } from '../_actions'
+import { toast } from 'sonner'
+import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons'
 
 export const GetInTouch = () => {
+  const [formState, formAction] = useFormState(sendEmail, { success: null })
+  const formRef = React.useRef<HTMLFormElement>(null)
+
+  React.useEffect(() => {
+    if (formState.success === false) {
+      toast.error(formState.message)
+    } else if (formState.success === true) {
+      toast.success(formState.message)
+      formRef.current?.reset()
+    }
+  }, [formState])
+
   return (
     <section className='w-full bg-transparent dark:bg-transparent py-12 md:py-24'>
       <div className='container px-4 md:px-6'>
@@ -16,30 +34,31 @@ export const GetInTouch = () => {
               Have a question or want to work together? Fill out the form below or reach out through my social channels.
             </p>
           </div>
-          <form className='space-y-4 text-left'>
+          <form ref={formRef} action={formAction} className='space-y-4 text-left'>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
               <div className='space-y-2'>
                 <Label htmlFor='name'>Name</Label>
-                <Input id='name' placeholder='Enter your name' />
+                <Input id='name' name='name' placeholder='Enter your name' required />
               </div>
               <div className='space-y-2'>
                 <Label htmlFor='email'>Email</Label>
-                <Input id='email' placeholder='Enter your email' type='email' />
+                <Input id='email' name='email' placeholder='Enter your email' type='email' required />
               </div>
             </div>
             <div className='space-y-2'>
               <Label htmlFor='message'>Message</Label>
-              <Textarea className='min-h-[150px]' id='message' placeholder='Enter your message' />
+              <Textarea className='min-h-[150px]' id='message' name='message' placeholder='Enter your message' required />
             </div>
-            <Button className='w-full' type='submit'>
-              Send Message
-            </Button>
+            <p className='sr-only' aria-live='polite' role='status'>
+              {formState.message}
+            </p>
+            <SubmitButton className='w-full'>Send Message</SubmitButton>
           </form>
           <div className='flex flex-col md:flex-row items-center justify-center gap-6'>
             <div className='space-y-2'>
               <div className='font-medium'>Chanin Lawlertrattana</div>
               <div className='text-gray-500 dark:text-gray-400'>
-                <a className='hover:underline' href='#'>
+                <a className='hover:underline' href='#' onClick={() => toast.info('Try calling me 🤷‍♂️')}>
                   +66 (635) 032-466
                 </a>
               </div>
