7325de7f5076e96e50ab197fc7181737b0cef4d0
Chanin Law <chaninlawlert@gmail.com>
2024-04-29 04:21:53 +0700

    style: color on extension items

--- components/editor/sidebar-items/editor-sidebar-extensions.tsx
+++ components/editor/sidebar-items/editor-sidebar-extensions.tsx
@@ -28,16 +28,18 @@ export function EditorSidebarExtensions() {
             </AccordionTrigger>
             <AccordionContent>
               <Link href={'/meta-llama'}>
-                <Alert className='border-none rounded-none hover:bg-slate-300/20 dark:hover:bg-zinc-100/10'>
-                  <FaMeta className='h-4 w-4' />
-                  <AlertTitle className='text-sm'>Meta-Llama-3-8B-Instruct</AlertTitle>
-                  <AlertDescription className='text-xs text-nowrap'>
-                    Meta developed and released the Meta Llama 3 family of large language models (LLMs)
-                  </AlertDescription>
-                  <AlertDescription className='text-xs flex items-center'>
-                    <TbDiscountCheckFilled />
-                    meta-llama
-                  </AlertDescription>
+                <Alert className='flex space-x-2 border-none rounded-none hover:bg-slate-300/20 dark:hover:bg-zinc-100/10'>
+                  <FaMeta className='h-5 w-5' />
+                  <div className='text-sm text-nowrap'>
+                    <AlertTitle>Meta-Llama-3-8B-Instruct</AlertTitle>
+                    <AlertDescription className='text-xs text-muted-foreground'>
+                      Meta developed and released the Meta Llama 3 family of large language models (LLMs)
+                    </AlertDescription>
+                    <AlertDescription className='text-xs flex items-center gap-1'>
+                      <TbDiscountCheckFilled />
+                      meta
+                    </AlertDescription>
+                  </div>
                 </Alert>
               </Link>
             </AccordionContent>
