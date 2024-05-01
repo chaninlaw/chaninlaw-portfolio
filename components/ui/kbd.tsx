d6cc27b9fc9c00bc65e1fa5cc715e4e7ab506033
Chanin Law <chaninlawlert@gmail.com>
2024-05-01 14:04:51 +0700

    fix: not found on dark mode

--- components/ui/kbd.tsx
+++ components/ui/kbd.tsx
@@ -1,6 +1,6 @@
 export const Kbd = ({ children }: { children?: React.ReactNode }) => {
   return (
-    <span className='h-6 w-6 flex justify-center items-center text-stone-500 bg-stone-800 rounded-md'>
+    <span className='h-6 w-6 flex justify-center items-center dark:text-stone-500 dark:bg-stone-800 rounded-md shadow-md'>
       <kbd className='font-sans tracking-widest'>{children}</kbd>
     </span>
   )
